bab6e371b96db45a8422f2347767d5cb60ac30a5
Clayton A. Alves <clayton.aa@gmail.com>
2014-03-06 22:41:00 -0400

    Separei as rotinas com acesso ao banco de dados no script database.py

---
+++ database.py
@@ -0,0 +1,89 @@
+#coding: utf8
+import MySQLdb
+
+conn = MySQLdb.connect('127.0.0.1', 'claytontemp', 'nonono', 'vigo')
+cursor = conn.cursor()
+
+class Titulo:
+    pass
+
+class Empresa:
+
+    def __init__(self, idempresa):
+        # Obtém os dados da empresa
+        cursor.execute('select fantasia, cidade, uf, telefone from empresas where id=%s' % idempresa)
+
+        fantasia, cidade_emp, uf, telefone_empresa = cursor.fetchone()
+        cidade_emp = "%s - %s" % (cidade_emp, uf)
+
+        self.idempresa = idempresa
+        self.fantasia  = fantasia.decode('latin1')
+        self.cidade    = cidade_emp.decode('latin1')
+        self.uf        = uf
+        self.telefone  = telefone_empresa
+
+    def titulos_atrasados(self):
+        idempresa = 1
+        situacao  = "''"
+        vcto1     = '2014-01-01'
+        vcto2     = '2014-03-31'
+        atraso1   = 10
+        atraso2   = 150
+        titulos1  = 1
+        titulos2  = 5
+
+        query = ( "select                                                                        "
+                  "   bol.nnumero,                                                               "
+                  "   bol.vcto,                                                                  "
+                  "   bol.valor,                                                                 "
+                  "   bol.nome,                                                                  "
+                  "   t1.cidade,                                                                 "
+                  "   t1.endereco,                                                               "
+                  "   t1.numero,                                                                 "
+                  "   t1.bairro,                                                                 "
+                  "   t1.telefone                                                                "
+                  "from boletos bol                                                              "
+                  "left join                                                                     "
+                  "    (   select                                                                "
+                  "            u.numero,                                                         "
+                  "            u.bairro,                                                         "
+                  "            u.telefone,                                                       "
+                  "            u.endereco,                                                       "
+                  "            u.cidade,                                                         "
+                  "            count(*) as titulos_abertos                                       "
+                  "        from boletos b                                                        "
+                  "        left join usuarios u using(numero)                                    "
+                  "        where                                                                 "
+                  "            u.idempresa=%(idempresa)d and                                     "
+                  "            u.situacao in (%(situacao)s) and                                  "
+                  "            b.pago='0' and                                                    "
+                  "            b.vcto between '%(vcto1)s' and '%(vcto2)s'                        "
+                  "        group by (u.numero)                                                   "
+                  "        order by u.nome                                                       "
+                  "    ) t1 on t1.numero=bol.numero                                              "
+                  "where                                                                         "
+                  "    bol.pago='0'                                                              "
+                  "    and ((datediff(curdate(), bol.vcto)) between %(atraso1)d and %(atraso2)d) "
+                  "    and (t1.titulos_abertos between %(titulos1)d and %(titulos2)d)            "
+                  "order by bol.numero, bol.vcto                                                 " )
+        query = query % (locals())
+
+        cursor.execute(query)
+
+        for record in cursor:
+            nnumero, vcto, valor, nome, cidade, endereco, numero, bairro, telefone = record
+
+            titulo          = Titulo()
+            titulo.nnumero  = nnumero
+            titulo.vcto     = vcto.strftime('%d/%m/%Y')
+            titulo.valor    = str(valor).replace('.', ',')
+            titulo.nome     = nome.decode('latin1')
+            titulo.numero   = numero
+            titulo.cidade   = cidade.decode('latin1')
+            titulo.endereco = endereco.decode('latin1')
+            titulo.bairro   = bairro.decode('latin1')
+            titulo.telefone = telefone
+
+            yield titulo
+
+
--- gera_cartas.py
+++ gera_cartas.py
@@ -1,121 +1,77 @@
 #coding: utf8
-import MySQLdb
 import datetime
 from string import Template
-from pprint import pprint
+from database import *
 
-IDEMPRESA = '1'
-atraso = datetime.timedelta(days=15)
-vcto_inicial = '1900-01-01'
-vcto_final   = (datetime.datetime.now()-atraso).strftime('%Y-%m-%d')
-taxa_religamento = '10,00'
-
-# Rio Cable/Som Sat
-conn = MySQLdb.connect('127.0.0.1', 'claytontemp', 'nonono', 'vigo')
-q = conn.cursor()
-
-# Obtém os dados da empresa
-q.execute('select fantasia, cidade, uf, telefone from empresas where id=%s' % IDEMPRESA)
-
-fantasia, cidade_emp, uf, telefone_empresa = q.fetchone()
-cidade_emp = "%s - %s" % (cidade_emp, uf)
-
-query = """\
-select bol.nnumero, bol.vcto, bol.valor, bol.nome, t1.cidade, t1.endereco, t1.numero, t1.bairro, t1.telefone from 
-	(select distinct
-		u.numero,
-		u.bairro,
-		u.telefone,
-        u.endereco,
-        u.cidade
-	from boletos b
-	left join usuarios u using(numero)
-	where 
-		u.idempresa=%s and
-		u.situacao='' and
-		b.pago='0' and
-		b.vcto between '%s' and '%s'
-	order by u.nome) t1
-left join boletos bol on t1.numero=bol.numero
-where bol.pago='0' and bol.vcto<curdate()
-order by nome, vcto
-""" % (IDEMPRESA, vcto_inicial, vcto_final)
-
-base = open('base.html', 'r')
-base_template = base.read().decode('utf8')
-base.close()
-
-template = open('template.html', 'r').read().decode('utf8')
-
-q.execute(query)
+TAXA_RELIGAMENTO = '10,00'
 
 class Cliente:
     def __repr__(self):
         return u'%d -  %s' % (self.numero, repr(self.boletos))
 
-clientes = {}
+def gerar(idempresa):
+    empresa = Empresa(idempresa)
 
-for nnumero, vcto, valor, nome, cidade, endereco, numero, bairro, telefone in q:
-    vencimento = vcto.strftime('%d/%m/%Y')
-    valor = str(valor).replace('.', ',')
+    base = open('base.html', 'r')
+    base_template = base.read().decode('utf8')
+    base.close()
 
-    if numero in clientes:
-        clientes[numero].boletos.append((nnumero, vencimento, valor))
-        continue
+    template = open('template.html', 'r').read().decode('utf8')
 
-    cliente = Cliente()
-    cliente.numero = numero
-    cliente.nnumero  = nnumero  
-    cliente.vcto     = vcto     
-    cliente.valor    = valor    
-    cliente.nome     = nome.decode('latin1')
-    cliente.cidade   = cidade.decode('latin1')
-    cliente.endereco = endereco.decode('latin1') 
-    cliente.bairro   = bairro.decode('latin1')
-    cliente.telefone = telefone 
-    cliente.boletos  = []
-    cliente.boletos.append((nnumero, vencimento, valor))
+    clientes = {}
 
-    clientes[numero] = cliente
+    for titulo in empresa.titulos_atrasados():
+        if titulo.numero in clientes:
+            clientes[titulo.numero].boletos.append(titulo)
+            continue
 
+        cliente = Cliente()
+        cliente.numero   = titulo.numero
+        cliente.nnumero  = titulo.nnumero  
+        cliente.vcto     = titulo.vcto     
+        cliente.valor    = titulo.valor    
+        cliente.nome     = titulo.nome
+        cliente.cidade   = titulo.cidade
+        cliente.endereco = titulo.endereco
+        cliente.bairro   = titulo.bairro
+        cliente.telefone = titulo.telefone 
+        cliente.boletos  = []
+        cliente.boletos.append(titulo)
 
-cartas = []
-boletos_tpl = Template('<p class="mono">$nnumero $vencimento    $valor</p>')
+        clientes[titulo.numero] = cliente
 
-for numero, cliente in clientes.iteritems():
-    s = Template(template)
+    cartas = []
+    boletos_tpl = Template('<p class="mono">$nnumero $vencimento    $valor</p>')
 
-    boletos = []
-    for boleto in cliente.boletos:
-        boletos.append(boletos_tpl.substitute(
-            nnumero    = boleto[0],
-            vencimento = boleto[1],
-            valor      = boleto[2]))
+    for numero, cliente in clientes.iteritems():
+        s = Template(template)
 
-    fantasia = fantasia.decode('latin1')
-    cidade   = cidade_emp.decode('latin1')
-    nome_cliente = cliente.nome
-    endereco = cliente.endereco
+        boletos = []
+        for boleto in cliente.boletos:
+            boletos.append(boletos_tpl.substitute(
+                nnumero    = titulo.nnumero,
+                vencimento = titulo.vcto,
+                valor      = titulo.valor))
 
-    saida = s.substitute(
-        nome_empresa = fantasia,
-        cidade       = cidade,
-        data         = datetime.datetime.now().strftime('%d/%m/%Y'),
-        nome_cliente = nome_cliente,
-        contrato = str(cliente.numero).zfill(5),
-        endereco = endereco,
-        bairro = cliente.bairro,
-        telefone = cliente.telefone,
-        telefone_empresa=telefone_empresa,
-        logofile='logo_empresa_%s.jpg' % IDEMPRESA,
-        taxa_religamento=taxa_religamento,
-        lista_boletos=''.join(boletos))
-    cartas.append(saida)
+        saida = s.substitute(
+            nome_empresa     = empresa.fantasia,
+            cidade           = empresa.cidade,
+            data             = datetime.datetime.now().strftime('%d/%m/%Y'),
+            nome_cliente     = cliente.nome,
+            contrato         = str(cliente.numero).zfill(5),
+            endereco         = cliente.endereco,
+            bairro           = cliente.bairro,
+            telefone         = cliente.telefone,
+            telefone_empresa = empresa.telefone,
+            logofile         = 'logo_empresa_%s.jpg' % empresa.idempresa,
+            taxa_religamento = TAXA_RELIGAMENTO,
+            lista_boletos    = ''.join(boletos))
+        cartas.append(saida)
 
-final = "".join(cartas)
+    final = "".join(cartas)
 
-output = base_template.replace('%{body}s', final)
+    output = base_template.replace('%{body}s', final)
 
-f = open('output.html', 'w')
-f.write(output.encode('utf8'))
-f.close()
+    f = open('output.html', 'w')
+    f.write(output.encode('utf8'))
+    f.close()
