7f7906f645676b7bae0ccab57b8f4a61e310b21d
Clayton A. Alves <clayton.aa@gmail.com>
2014-03-11 20:13:53 -0400

    Novo modulo para geração das cartas em html e pdf

---
+++ cartas.py
@@ -0,0 +1,72 @@
+import os
+import subprocess
+import datetime
+from string import Template
+
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+
+TAXA_RELIGAMENTO = 1.1
+PAGAR_ATE        = datetime.datetime.now()+datetime.timedelta(days=15)
+DATA_BLOQUEIO    = datetime.datetime.now()+datetime.timedelta(days=20)
+
+class CartasCobranca:
+
+    def __init__(self, empresa):
+        self.cartas = []
+        self.empresa = empresa
+
+    def append(self, cliente):
+        template = open('%s/template.html' % dir_path, 'r').read().decode('utf8')
+        s = Template(template)
+
+        boletos_tpl = Template('$nnumero  $vencimento  $valor')
+
+        titulos = []
+        for titulo in cliente.boletos:
+            titulos.append(boletos_tpl.substitute(
+                nnumero    = titulo.nnumero,
+                vencimento = titulo.vencimento.strftime('%d/%m/%Y'),
+                valor      = ("%.2f" % titulo.valor).replace('.', ',')))
+
+        saida = s.substitute(
+            nome_empresa     = self.empresa.fantasia,
+            cidade           = self.empresa.cidade,
+            data             = datetime.datetime.now().strftime('%d/%m/%Y'),
+            nome_cliente     = cliente.nome,
+            contrato         = str(cliente.numero).zfill(5),
+            endereco         = cliente.endereco,
+            bairro           = cliente.bairro,
+            telefone         = cliente.telefone,
+            telefone_empresa = self.empresa.telefone,
+            logofile         = '%s/logo_empresa_%s.jpg' % (dir_path, self.empresa.idempresa),
+            taxa_religamento = TAXA_RELIGAMENTO,
+            lista_boletos    = '\n'.join(titulos),
+            pagar_ate        = PAGAR_ATE.strftime('%d/%m/%Y'),
+            data_bloqueio    = DATA_BLOQUEIO.strftime('%d/%m/%Y'))
+        self.cartas.append(saida)
+
+    def gerar(self):
+        """ Retornar nome do arquivo .pdf para iniciar download
+        """
+        base = open('%s/base.html' % dir_path, 'r')
+        base_template = base.read().decode('utf8')
+        base.close()
+
+        final = "".join(self.cartas)
+
+        output = base_template.replace('%{body}s', final)
+
+        f = open('output.html', 'w')
+        f.write(output.encode('utf8'))
+        f.close()
+
+        self._geraPDF()
+
+    def _geraPDF(self):
+        """ Gera o arquivo .pdf a partir do .html
+        """
+        FNULL = open(os.devnull, 'w')
+        retcode = subprocess.call(["wkhtmltopdf-amd64", "-q", "output.html", "output.pdf"], stdout=FNULL, stderr=FNULL)
+        
+
---
+++ tests/testCartas.py
@@ -0,0 +1,94 @@
+""" This is a really dummy test to check if the script
+    is generating the html and pdf correctly.
+"""
+
+import sys
+import datetime
+from collections import namedtuple
+
+sys.path.append('..')
+from cartas import CartasCobranca
+
+empresa = namedtuple('Empresa', 'idempresa, fantasia, cidade, telefone')
+cliente = namedtuple('Cliente', 'numero, nome, endereco, bairro, telefone, boletos')
+
+empresa.idempresa = 1
+empresa.fantasia = 'Clayton Co'
+empresa.cidade = 'Alta Floresta - MT'
+empresa.telefone = '3333-3333'
+cartas = CartasCobranca(empresa)
+
+# Cria um cliente com um titulo
+cliente.numero = 1234
+cliente.nome = 'Clayton de Almeida Alves'
+cliente.endereco = 'Rua das flores'
+cliente.bairro = 'Centro'
+cliente.telefone = '1234-4321'
+cliente.boletos = []
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000001-1'
+titulo.vencimento = datetime.datetime.now()
+titulo.valor = 42.42
+cliente.boletos.append(titulo)
+cartas.append(cliente)
+
+# Cria um cliente com dois titulos
+cliente.numero = 4321
+cliente.nome = 'Vanessa A. Mendonca'
+cliente.endereco = 'Rua das Oliveiras'
+cliente.bairro = 'Bom Pastor'
+cliente.telefone = '4321-4321'
+cliente.boletos = []
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000002-1'
+titulo.vencimento = datetime.datetime.now()
+titulo.valor = 10.0
+cliente.boletos.append(titulo)
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000003-1'
+titulo.vencimento = datetime.datetime.now()+datetime.timedelta(days=1)
+titulo.valor = 20
+cliente.boletos.append(titulo)
+cartas.append(cliente)
+
+
+# Cria um cliente com quatro titulos
+cliente.numero = 4321
+cliente.nome = 'Cliente Devedor'
+cliente.endereco = 'Rua das Bananeiras'
+cliente.bairro = 'Testando'
+cliente.telefone = '1111-1111'
+cliente.boletos = []
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000004-1'
+titulo.vencimento = datetime.datetime.now()
+titulo.valor = 10.0
+cliente.boletos.append(titulo)
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000005-1'
+titulo.vencimento = datetime.datetime.now()+datetime.timedelta(days=1)
+titulo.valor = 20
+cliente.boletos.append(titulo)
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000006-1'
+titulo.vencimento = datetime.datetime.now()+datetime.timedelta(days=2)
+titulo.valor = 20
+cliente.boletos.append(titulo)
+
+titulo = namedtuple('Titulo', 'nnumero, vencimento, valor')
+titulo.nnumero = '00000000007-1'
+titulo.vencimento = datetime.datetime.now()+datetime.timedelta(days=3)
+titulo.valor = 20
+cliente.boletos.append(titulo)
+cartas.append(cliente)
+
+# Gera as cartas
+cartas.gerar()
+
+
